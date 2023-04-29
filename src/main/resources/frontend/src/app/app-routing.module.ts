24b02b4d292df873c60e82a71ee9b8d0010761c0
Nizar <nizar.al.nasraoui@gmail.com>
2023-04-29 14:18:10 +0200

    refactoring

--- src/main/resources/frontend/src/app/app-routing.module.ts
+++ src/main/resources/frontend/src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { AuthGardService as AuthGard}  from './core/_services/auth-gard.service'
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./core/auth/auth.module').then(m => m.AuthModule) },
-  {path:'passwords',loadChildren: () => import('./password-manager/password-manager.module').then(m => m.PasswordManagerModule),canActivate:[AuthGard]},
+  {path:'passwords',loadChildren: () => import('./password-manager/password-manager.module').then(m => m.PasswordManagerModule)//,canActivate:[AuthGard]
+},
   { path: '**', redirectTo: 'pages/notfound' },
 
 ];
